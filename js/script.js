c7a04368a78de5de01c95c200af31836dc23bf26
Ilya Pronyakin <blastinboosting@gmail.com>
2023-09-26 16:09:47 +0300

    Blast

--- js/script.js
+++ js/script.js
@@ -16,3 +16,25 @@ button.addEventListener("click", function(event) {
 
 	
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
