1df2e0f85f7ffb97de7d248785417d84188e0935
Ilya Pronyakin <blastinboosting@gmail.com>
2023-09-28 00:48:15 +0300

    blast

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let nav = document.querySelector(".nav-menu");
+
 let button = document.querySelector(".button");
 
 let hiddenItems = document.querySelectorAll(".nav-menu__item--hidden");
@@ -17,14 +19,18 @@ button.addEventListener("click", function(event) {
 	
 });
 
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
+  spaceBetween: 16,
+  slidesPerView: 1.2,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -37,4 +43,87 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+      breakpoints: {
+      321: {
+        slidesPerView: 1.3
+      },
+      430: {
+        slidesPerView: 1.7
+      },
+      620: {
+        slidesPerView: 2.4
+      }
+    }
+
+
+
 });
+
+// let swiper1 = document.querySelector(".swiper");
+
+// var init = false;
+// function swiperCard() {
+//   if (window.innerWidth <= 768) {
+//     if (!init) {
+//       init = true;
+//       
+//     }
+//   } else if (init) {
+
+//     init = false;
+//     
+//   }  
+// }
+// swiperCard();
+// window.addEventListener("resize", swiperCard);
+
+
+
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+//     let swiper
+
+//     breakpoint = window.matchMedia(breakpoint)
+
+//     const enableSwiper = function (className, settings) {
+//       swiper = new Swiper(className, settings)
+//     }
+
+//     const checker = function () {
+//       if (breakpoint.matches) {
+//         return enableSwiper(swiperClass, swiperSettings)
+//       } else {
+//         if (swiper !== undefined) swiper.destroy(true, true)
+//         return
+//       }
+//     }
+
+//     breakpoint.addEventListener('change', checker)
+//     checker()
+//   }
+
+//   //brands
+
+//   resizableSwiper('(max-width: 767px)', '.swiper', {
+//     spaceBetween: 16,
+//     slidesPerView: 1.2,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       321: {
+//         slidesPerView: 1.3
+//       },
+//       430: {
+//         slidesPerView: 1.7
+//       },
+//       620: {
+//         slidesPerView: 2.4
+//       }
+//     }
+//   })
+// })  
